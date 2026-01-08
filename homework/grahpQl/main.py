37e35a69ea354b971cd6b7c4ad8c0f0ea7514e0e
조대건 <cdk0825@nate.com>
2026-01-08 17:34:03 +0900

    api 서버 파이선환경 실행  테스트2

--- homework/grahpQl/main.py
+++ homework/grahpQl/main.py
@@ -7,6 +7,7 @@ from typing import List
 python -m strawberry dev main 서버 실행 명령어실행
 http://0.0.0.0:8000/graphql 으로 실행하라고 설명하지만
 실질적으로 http://loalhost:8000/graphql 으로 실행해야 접속가능함
+또는 http://127.0.0.1:8000/graphql 으로 접속
 '''
 
 # 1. 데이터 구조 정의(Schema)
@@ -34,7 +35,66 @@ class Query:
             if user.name == name:
                 return user
         return None
+    
+    # 🔴 여기 (age: int = None) 이 부분이 반드시 추가되어야 합니다!
+    @strawberry.field
+    def get_users(self, age: int = None) -> List[User]:
+        if age is not None:
+            return [user for user in USER_DATA if user.age == age]
+        return USER_DATA
+    
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def add_user(self, name: str, age: int) -> User:
+        new_user = User(name=name, age=age)
+        USER_DATA.append(new_user)
+        return new_user
 
 # 3. 스키마 및 서버 설정
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(query=Query, mutation=Mutation)
     
+    
+'''
+브라우저 테스트 
+- 쿼리 입력값
+
+(나이)
+query FilteredUsers($inputAge: Int) {
+  getUsers(age: $inputAge) {
+    name
+    age
+  }
+}
+(이름)
+query FilteredUsers($inputName: String!) {
+  searchUser(name: $inputName) {
+    name
+    age
+  }
+}
+
+-Variable 입력값
+(나이)
+{
+  "inputAge": 30
+}
+(이름)
+{
+  "inputName": "홍길동"
+}
+
+
+-데이터 추가
+mutation CreateNewUser($newName: String!, $newAge: Int!) {
+  addUser(name: $newName, age: $newAge) {
+    name
+    age
+  }
+}
+
+{
+  "newName": "김철수",
+  "newAge": 25
+}
+'''
